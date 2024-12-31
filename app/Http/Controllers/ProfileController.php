11e2ba4c982936ddf181454b3c024b9492fe2d78
mahbub-shohag <shohag.cse3@gmail.com>
2024-12-31 17:18:08 +0600

    profile update api modified

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -98,50 +98,53 @@ class ProfileController extends Controller
 
     public function updateProfileAPI(Request $request)
     {
-        $user = Auth::user();
-        $profile = Profile::where('user_id', $user->id)->first();
-//        print_r($request->data);exit;
-        $info = json_decode($request->data);
-//        print_r($info->fullName);exit;
-        $profile->fullName = $info->fullName ?? $profile->fullName;
-        $profile->phoneNumber = $info->phoneNumber ?? $profile->phoneNumber;
-        $profile->religion = $info->religion ?? $profile->religion;
-        $profile->education = $info->education ?? $profile->education;
-        $profile->occupation = $info->occupation ?? $profile->occupation;
-        $profile->presentDivision = $info->presentDivision ?? $profile->presentDivision;
-        $profile->presentDistrict = $info->presentDistrict ?? $profile->presentDistrict;
-        $profile->presentUpazilla = $info->presentUpazilla ?? $profile->presentUpazilla;
-        $profile->presentCity = $info->presentCity ?? $profile->presentCity;
-        $profile->permanentDivision = $info->permanentDivision ?? $profile->permanentDivision;
-        $profile->permanentDistrict = $info->permanentDistrict ?? $profile->permanentDistrict;
-        $profile->permanentUpazilla = $info->permanentUpazilla ?? $profile->permanentUpazilla;
-        $profile->permanentCity = $info->permanentCity ?? $profile->permanentCity;
-        $profile->preferableDivision = $info->preferableDivision ?? $profile->preferableDivision;
-        $profile->preferableDistrict = $info->preferableDistrict ?? $profile->preferableDistrict;
-        $profile->preferableUpazilla = $info->preferableUpazilla ?? $profile->preferableUpazilla;
-        $profile->preferableCity = $info->preferableCity ?? $profile->preferableCity;
-        $profile->estimatedBudget = $info->estimatedBudget ?? $profile->estimatedBudget;
-        $profile->preferableFlatSize = $info->preferableFlatSize ?? $profile->preferableFlatSize;
-        $profile->monthlyIncome = $info->monthlyIncome ?? $profile->monthlyIncome;
-        $profile->currentCapital = $info->currentCapital ?? $profile->currentCapital;
-        $profile->totalFamilyMembers = $info->totalFamilyMembers ?? $profile->totalFamilyMembers;
-        $profile->sourceOfIncome = $info->sourceOfIncome ?? $profile->sourceOfIncome;
-        $profile->user_id = $user->id; // Assuming the user ID is always set.
-        $profile->age = $info->age ?? $profile->age;
+        echo "<pre>";
+        print_r($request->profilePhoto);
+        try {
+            $user = Auth::user();
+            $profile = Profile::where('user_id', $user->id)->first();
+            $info = json_decode($request->data);
+            $profile->fullName = $info->fullName ?? $profile->fullName;
+            $profile->phoneNumber = $info->phoneNumber ?? $profile->phoneNumber;
+            $profile->religion = $info->religion ?? $profile->religion;
+            $profile->education = $info->education ?? $profile->education;
+            $profile->occupation = $info->occupation ?? $profile->occupation;
+            $profile->presentDivision = $info->presentDivision ?? $profile->presentDivision;
+            $profile->presentDistrict = $info->presentDistrict ?? $profile->presentDistrict;
+            $profile->presentUpazilla = $info->presentUpazilla ?? $profile->presentUpazilla;
+            $profile->presentCity = $info->presentCity ?? $profile->presentCity;
+            $profile->permanentDivision = $info->permanentDivision ?? $profile->permanentDivision;
+            $profile->permanentDistrict = $info->permanentDistrict ?? $profile->permanentDistrict;
+            $profile->permanentUpazilla = $info->permanentUpazilla ?? $profile->permanentUpazilla;
+            $profile->permanentCity = $info->permanentCity ?? $profile->permanentCity;
+            $profile->preferableDivision = $info->preferableDivision ?? $profile->preferableDivision;
+            $profile->preferableDistrict = $info->preferableDistrict ?? $profile->preferableDistrict;
+            $profile->preferableUpazilla = $info->preferableUpazilla ?? $profile->preferableUpazilla;
+            $profile->preferableCity = $info->preferableCity ?? $profile->preferableCity;
+            $profile->estimatedBudget = $info->estimatedBudget ?? $profile->estimatedBudget;
+            $profile->preferableFlatSize = $info->preferableFlatSize ?? $profile->preferableFlatSize;
+            $profile->monthlyIncome = $info->monthlyIncome ?? $profile->monthlyIncome;
+            $profile->currentCapital = $info->currentCapital ?? $profile->currentCapital;
+            $profile->totalFamilyMembers = $info->totalFamilyMembers ?? $profile->totalFamilyMembers;
+            $profile->sourceOfIncome = $info->sourceOfIncome ?? $profile->sourceOfIncome;
+            $profile->user_id = $user->id; // Assuming the user ID is always set.
+            $profile->age = $info->age ?? $profile->age;
 
 
-        if ($request->hasFile('profilePhoto') && $request->file('profilePhoto')->isValid()) {
-            $profilePhoto_path = $request->file('profilePhoto')->store('profilePhotos', 'public');
-            print_r($profilePhoto_path);
+            if ($request->hasFile('profilePhoto') && $request->file('profilePhoto')->isValid()) {
+                $profilePhoto_path = $request->file('profilePhoto')->store('profilePhotos', 'public');
+                $profile->profilePhoto = $profilePhoto_path;
+            } else {
+                $profile->profilePhoto = $profile->profilePhoto ?? $profile->profilePhoto;
+            }
+            $profile->save();
 
-            $profile->profilePhoto = $profilePhoto_path;
-        } else {
-            $profile->profilePhoto = $profile->profilePhoto ?? $profile->profilePhoto;
+
+            return $profile;
+        }catch (\Exception $exception){
+            echo $exception->getMessage();
         }
-        $profile->save();
 
-
-        return $profile;
     }
 
 
